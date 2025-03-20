16b0995c742b2916eaa59251ef551ed608c04e56
Daan Gieles <daan.gieles@mobilityhouse.com>
2025-03-20 17:44:55 +0100

    desperate changes

--- optimization.py
+++ optimization.py
@@ -67,7 +67,7 @@ def run_single_optimization(starting_soc = 0.1, p_limit = 0.1,date='2025-01-15',
     #Run optimization
     
     # opt = pyo.SolverFactory('cbc')
-    opt = pyo.SolverFactory('ipopt')
+    opt = pyo.SolverFactory('ipopt',executable='./ipopt')
     # opt.options['output_file'] = "ipopt_log.txt"
     result = opt.solve(model)
     
