62ca4d5a13dc0a462471b51bccdce217cf8a48a7
Daan Gieles <daan.gieles@mobilityhouse.com>
2025-03-21 10:52:43 +0100

    trying without executable

--- optimization.py
+++ optimization.py
@@ -67,8 +67,9 @@ def run_single_optimization(starting_soc = 0.1, p_limit = 0.1,date='2025-01-15',
     #Run optimization
     
     # opt = pyo.SolverFactory('cbc')
-    # opt = pyo.SolverFactory('ipopt',executable='ipopt',solver_io='nl')
-    opt = pyo.SolverFactory('glpk')
+    # opt = pyo.SolverFactory('ipopt',executable='ipopt.exe',solver_io='nl')
+    opt = pyo.SolverFactory('ipopt')
+    # opt = pyo.SolverFactory('glpk')
     # opt.options['output_file'] = "ipopt_log.txt"
     result = opt.solve(model)
     
