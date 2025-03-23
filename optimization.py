d71f719a7e5205056df9204174e9209a41a4f6e7
Daan Gieles <daan.gieles@mobilityhouse.com>
2025-03-23 10:37:02 +0100

    gurobi solver and ETP factor in objective

--- optimization.py
+++ optimization.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 
-
+plt.style.use('ggplot')
 def generate_output(starting_soc = 0.1, p_limit = 0.1,date='2025-01-15',charge_from_grid=True,etp_cost = 5):
-    model_colocation,result_colocation = run_single_optimization_colocation(starting_soc = starting_soc, p_limit = p_limit,date=date,charge_from_grid=charge_from_grid,etp_cost=etp_cost)
-    model_battery,result_battery = run_single_optimization_colocation(starting_soc = starting_soc, p_limit = p_limit,date=date,charge_from_grid=charge_from_grid,no_pv=True,etp_cost=etp_cost)
+    model_colocation,_ = run_single_optimization_colocation(starting_soc = starting_soc, p_limit = p_limit,date=date,charge_from_grid=charge_from_grid,etp_cost=etp_cost)
+    model_battery,_ = run_single_optimization_colocation(starting_soc = starting_soc, p_limit = p_limit,date=date,charge_from_grid=charge_from_grid,no_pv=True,etp_cost=etp_cost)
     
     #Generate plots
     ts_series =list(model_colocation.t.ordered_data())
@@ -26,33 +26,34 @@ def generate_output(starting_soc = 0.1, p_limit = 0.1,date='2025-01-15',charge_f
     
     # ax1,ax2 = axes
 
-    ax1.plot(ts_series,soc_series,label='SOC')
-    ax1.plot(ts_series,pv_series,label='PV')
-    ax1.plot(ts_series,p_series,label='Power')
+    ax1.plot(ts_series,soc_series,label='SOC [MWh]')
+    ax1.plot(ts_series,pv_series,label='PV [MW]')
+    ax1.plot(ts_series,p_series,label='Power [MW]')
     ax2.plot(ts_series,accumulated_pnl_series_colocation,label=f'P&L Colocation: {round(accumulated_pnl_series_colocation[-1],2)}EU')
     ax2.plot(ts_series,accumulated_pnl_series_pv,label=f'P&L PV Only: {round(accumulated_pnl_series_pv[-1],2)}EU')
     ax2.plot(ts_series,accumulated_pnl_series_battery_only,label=f'P&L Battery Only: {round(accumulated_pnl_series_battery_only[-1],2)}EU')
     ax3.plot(ts_series,da_prices_series,label='DA prices')
     
-    ax1.set_ylabel('SOC/PV/Battery Power')
-    ax2.set_ylabel('DA prices')
-    ax3.set_ylabel('P&L')
+    ax1.set_ylabel('SOC / PV / Battery Power',fontsize=9)
+    ax2.set_ylabel('DA prices',fontsize=9)
+    ax3.set_ylabel('P&L',fontsize=9)
 
-    ax1.set_title(f'Single-shot optimization on {date}. Max power {str(round(p_limit,2))}, starting SOC {str(round(starting_soc,1))}')
+    ax1.set_title(f'Single-shot optimization on {date}. Max power {str(round(p_limit,2))}, starting SOC {str(round(starting_soc,1))}',fontsize=9)
 
     ax3.xaxis.set_major_locator(mdates.HourLocator())
     ax3.xaxis.set_major_formatter(mdates.DateFormatter('%H:00'))
     ax3.xaxis.set_minor_locator(mdates.MinuteLocator(byminute=[15,30,45]))
     # plt.show()
-    ax3.tick_params(rotation=45,labelsize=7,axis='x')
+    ax3.tick_params(rotation=45,labelsize=9,axis='x')
     ax1.legend()
     ax2.legend()
     ax3.legend()
 
     plt.tight_layout()
     return fig
-def run_single_optimization_colocation(starting_soc = 0.1, p_limit = 0.1,date='2025-01-15',charge_from_grid=True,no_pv=False,etp_cost=5):
 
+def run_single_optimization_colocation(starting_soc = 0.1, p_limit = 0.1,date='2025-01-15',charge_from_grid=True,no_pv=False,etp_cost=5):
+    
     def soc_calculation(model,t):
         preceding_t = [ts for ts in model.t if ts<=t]
         return starting_soc+np.sum([model.p[t] for t in preceding_t])/4
@@ -80,8 +81,8 @@ def run_single_optimization_colocation(starting_soc = 0.1, p_limit = 0.1,date='2
     
     def objective_expression(model):
     
-        # return sum(-model.da[t]*(model.p[t]-model.pv[t]) for t in model.t)-sum(model.p[t]**2 for t in model.t)*ETP_COST/4
-        return sum(-model.da[t]*(model.p[t]-model.pv[t]) for t in model.t)
+        return sum(-model.da[t]*(model.p[t]-model.pv[t]) for t in model.t)-sum(model.p[t]**2 for t in model.t)*etp_cost/4
+        # return sum(-model.da[t]*(model.p[t]-model.pv[t]) for t in model.t)
     model = pyo.ConcreteModel()
     df = prepare_data.merge_prices_and_solar(date=date)
     
@@ -108,7 +109,7 @@ def run_single_optimization_colocation(starting_soc = 0.1, p_limit = 0.1,date='2
     model.constraint_p_min = pyo.Constraint(model.t,expr=constraint_p_min)
     model.constraint_soc_lower = pyo.Constraint(model.t,expr=constraint_soc_lower)
     model.constraint_soc_upper = pyo.Constraint(model.t,expr=constraint_soc_upper)
-    # model.constraint_etp = pyo.Constraint(expr=constraint_etp)
+
     #Objective
     model.OBJ = pyo.Objective(rule=objective_expression,sense=pyo.maximize)
     
@@ -117,11 +118,10 @@ def run_single_optimization_colocation(starting_soc = 0.1, p_limit = 0.1,date='2
 
     #Run optimization
     
-    opt = pyo.SolverFactory('appsi_highs')
+    # opt = pyo.SolverFactory('appsi_highs')
+    opt = pyo.SolverFactory('gurobi')
 
     result = opt.solve(model)
-    
-    
     return model,result
 
 
--- prepare_data.py
+++ prepare_data.py
@@ -1,25 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-def average_solar_production():
-    df = pd.read_csv('example_data/solar/solar_generation_germany.csv')
-    
-    df['start_ts'] = pd.to_datetime(df['MTU'].str.split(' - ').str[0],format='%d.%m.%Y %H:%M')
-    df.rename({'Solar - Actual Aggregated [MW]':'solar_production_mw'},axis='columns',inplace=True)
-    df = df[df['solar_production_mw']!='-'].dropna(subset=['solar_production_mw'])
-
-    df['solar_production_mw'] = df['solar_production_mw'].astype(int) 
-    df = df[['start_ts','solar_production_mw']]
-    
-    df = df.groupby(by=df['start_ts'].dt.time)['solar_production_mw'].mean().reset_index(drop=False)
-
-    df['solar_production_mw'] = df['solar_production_mw']/df['solar_production_mw'].max() 
-    # print(df)
-    # fig,ax = plt.subplots()
-
-    # ax.plot(df['solar_production_mw'])
-    df['start_ts'] = '2025-03-18T' + df['start_ts'].astype(str)
-    df['start_ts'] = pd.to_datetime(df['start_ts'])
-    return df
 
 def estimate_solar_production(date='2025-01-05'):
     
@@ -32,13 +12,8 @@ def estimate_solar_production(date='2025-01-05'):
     df['solar_production_mw'] = df['solar_production_mw'].astype(int) 
     df = df[['start_ts','solar_production_mw']]
     
-    # df = df.groupby(by=df['start_ts'].dt.time)['solar_production_mw'].mean().reset_index(drop=False)
-
-    # print(df)
-    # fig,ax = plt.subplots()
-    # ax.plot(df['solar_production_mw'])
-    df = df[df['start_ts'].dt.strftime('%Y-%m-%d')==date]
     df['solar_production_mw'] = df['solar_production_mw']/df['solar_production_mw'].max() 
+    df = df[df['start_ts'].dt.strftime('%Y-%m-%d')==date]
 
     return df
 
@@ -47,21 +22,17 @@ def prepare_da_prices(date='2025-01-09'):
     df = df[df['Sequence']=='Sequence 1']
     
     df['start_ts'] = pd.to_datetime(df['MTU (CET/CEST)'].str.split(' - ').str[0],format='%d/%m/%Y %H:%M:%S')
-    #Filter the specified date
+
     df = df[df['start_ts'].dt.strftime('%Y-%m-%d')==date]
     
     df.rename({'Day-ahead Price (EUR/MWh)':'da_price'},axis='columns',inplace=True)
     df = df[['start_ts','da_price']]
-    print(df)
     return df
 
 def merge_prices_and_solar(date='2025-01-15'):
     df_prices = prepare_da_prices(date=date)
     df_solar = estimate_solar_production(date=date)
-    print(df_solar)
-    print(df_prices)
     df_merged = pd.merge(df_prices,df_solar,on='start_ts',how='inner')
-    print(df_merged)
     return df_merged
 if __name__ == "__main__":
     merge_prices_and_solar()
