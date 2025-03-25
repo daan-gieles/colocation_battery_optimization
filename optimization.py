22ae457b6e62c9d28001d74ad2d1c226005c2c20
Daan Gieles <daan.gieles@mobilityhouse.com>
2025-03-25 11:02:52 +0100

    comments and descriptions

--- optimization.py
+++ optimization.py
@@ -10,14 +10,14 @@ import calculate_kpis
 plt.style.use('ggplot')
 
 
-def generate_output(starting_soc=0.1, p_limit=0.1, date='2025-01-15', charge_from_grid=True, etp_cost=5, average_bid_ask_spread=1):
+def generate_output(battery_capacity=4, max_power_pv=8, starting_soc=0.1, p_limit=0.1, date='2025-01-15', charge_from_grid=True, etp_cost=5, average_bid_ask_spread=1):
     """
-    Runs the optimization, plots results
+    Does the optimization calls, plots results. Not as efficient as it could be...
     """
 
-    model_colocation, _ = run_single_optimization_colocation(
-        starting_soc=starting_soc, p_limit=p_limit, date=date, charge_from_grid=charge_from_grid, etp_cost=etp_cost, average_bid_ask_spread=average_bid_ask_spread)
-    model_battery, _ = run_single_optimization_colocation(starting_soc=starting_soc, p_limit=p_limit, date=date,
+    model_colocation, _ = run_single_optimization_colocation(battery_capacity=battery_capacity, max_power_pv=max_power_pv,
+                                                             starting_soc=starting_soc, p_limit=p_limit, date=date, charge_from_grid=charge_from_grid, etp_cost=etp_cost, average_bid_ask_spread=average_bid_ask_spread)
+    model_battery, _ = run_single_optimization_colocation(battery_capacity=battery_capacity, max_power_pv=max_power_pv, starting_soc=starting_soc, p_limit=p_limit, date=date,
                                                           charge_from_grid=charge_from_grid, no_pv=True, etp_cost=etp_cost, average_bid_ask_spread=average_bid_ask_spread)
 
     # Extract data from the optimization model
@@ -43,7 +43,7 @@ def generate_output(starting_soc=0.1, p_limit=0.1, date='2025-01-15', charge_fro
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True)
 
     ax1.set_title(
-        f'Single-shot optimization on {date}. Max power {str(round(p_limit, 2))}, starting SOC {str(round(starting_soc, 1))}', fontsize=9)
+        f'{date}. Battery: {str(round(p_limit, 1))}/{str(round(battery_capacity,2))} MW/MWh, Max PV Capacity: {str(round(max_power_pv,2))}MW. \n Starting SOC {str(round(starting_soc, 1))}%', fontsize=7)
 
     ax1.plot(ts_series, soc_series, label='SOC [MWh]')
     ax1.plot(ts_series, pv_series, label='PV [MW]')
@@ -82,38 +82,63 @@ def generate_output(starting_soc=0.1, p_limit=0.1, date='2025-01-15', charge_fro
     return fig
 
 
-def run_single_optimization_colocation(starting_soc=0.1, p_limit=0.1, date='2025-01-15', charge_from_grid=True, no_pv=False, etp_cost=5, average_bid_ask_spread=1):
-
+def run_single_optimization_colocation(battery_capacity=4, max_power_pv=8, starting_soc=0.1, p_limit=0.1, date='2025-01-15', charge_from_grid=True, no_pv=False, etp_cost=5, average_bid_ask_spread=1):
+    """
+    Runs a single optimization using passed battery/PV specifications.
+    """
     def soc_calculation(model, t):
+        """
+        Defines the calculation for the SOC at time-index t
+        """
         preceding_t = [ts for ts in model.t if ts <= t]
-        return starting_soc+np.sum([model.p[t] for t in preceding_t])/4
+        return battery_capacity*starting_soc/100+np.sum([model.p[t] for t in preceding_t])/4
 
     def constraint_p_max(model, t):
+        """
+        Constrains charging power of the battery. Depends on whether the battery is allowed to charge from the grid
+        """
         if charge_from_grid:
             return model.p[t] <= p_limit
         else:
             return model.p[t] <= model.pv[t]
 
     def constraint_p_min(model, t):
+        """
+        Constrains discharging power of the battery.
+        """
         return model.p[t] >= -p_limit
 
     def constraint_soc_lower(model, t):
+        """Sets lower limit of the SOC"""
         return model.soc[t] >= 0
 
     def constraint_soc_upper(model, t):
-        return model.soc[t] <= 1
+        """Sets upper limit of the SOC"""
+        return model.soc[t] <= battery_capacity
 
     def cashflow_rule_colocation(model, t):
+        """
+        Defines the cashflow calculation at time-index t
+        """
         return model.da[t]*(model.pv[t]-model.p[t])/4
 
     def cashflow_rule_only_pv(model, t):
+        """
+        Defines the cashflow calculation at time-index t of the PV-only scenario
+        """
         return model.da[t]*model.pv[t]/4
 
     # Trick for including an absolute value term in the objective
     def absolute_value_constraint(model, t):
+        """
+        Some trickery to get access to the absolute value in the objective function. t1[t]+t2[t] would always be equal to abs(p[t])
+        """
         return model.t1[t]-model.t2[t] == model.p[t]
 
     def objective_expression(model):
+        """
+        Defines the objective calculation. One term for the profit, one term that penalizes energy throughput in the battery
+        """
         return sum(-model.da[t]*(model.p[t]-model.pv[t]) for t in model.t)/4-sum((model.t1[t]+model.t2[t]) for t in model.t)*etp_cost/4
         # return sum(-model.da[t]*(model.p[t]-model.pv[t]) for t in model.t)
 
@@ -122,6 +147,9 @@ def run_single_optimization_colocation(starting_soc=0.1, p_limit=0.1, date='2025
 
     if no_pv:
         df['solar_production_mw'] = 0
+
+    # Scale the solar production to the production capacity
+    df['solar_production_mw'] *= max_power_pv
 
     df = df.set_index('start_ts', drop=True)
 
@@ -168,7 +196,3 @@ def run_single_optimization_colocation(starting_soc=0.1, p_limit=0.1, date='2025
     result = opt.solve(model)
     return model, result
 
-
-#
-if __name__ == "__main__":
-    generate_output()
--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import optimization
+import time
 
 def main():
+    """
+    Main streamlit logic
+    """
     #This allows bullet lists
     st.markdown('''
             <style>
@@ -14,13 +18,19 @@ def main():
     st.title('Optimization of co-located 1MW solar plant with a 1MWh battery')
     with st.expander("See explanation"):
         st.markdown("- This is a hobby project in which I try to estimate the combined returns of a PV+battery (1MW/1MWh) combination")
-        st.markdown("- You can select a battery power level, a starting state-of-charge (SOC) and the option whether the battery is allowed to charge from the grid instead of just from the PV")
-        st.markdown("- The ETP (energy throughput) cost which penalizes MWh charged/discharged")
+        st.markdown("- You can select a battery power level, a starting state-of-charge (SOC) and the option whether the battery is allowed to charge from the grid instead or solely from the PV")
+        st.markdown("- The ETP (energy throughput) cost penalizes MWh's charged/discharged by the battery")
         st.markdown("- You can run an optimization (using the Gurobi solver) using the EPEX day-ahead prices in Germany for the first ~3 months of 2025")
-        st.markdown("- Closing statements for now: this is no accurate representation of the value. To approximate it more accurately, one would also need to take into account the grid-fee-related benefits, the volatile intraday price development, the uncertainty of PV forecasts at the time of making trading decisions, the subsidy scheme in Germany, etc.")
+        st.markdown("- Closing statements for now: this is no accurate representation of the value. To approximate it more accurately, one would also need to take into account the grid-fee-related benefits, the volatile intraday price development, the uncertainty of PV forecasts at the time of making trading decisions, the subsidy scheme in Germany, etc. Furthermore, the optimization optimizes on QH basis while DA prices (as of March 2025) still correspond to hourly contracts.")
     
-    max_power = st.slider(value=0.5,label="Maximum absolute power limit [MW]",min_value=0.0,max_value=1.0,step=0.05)
-    starting_soc = st.slider(label="Starting SOC [MWh]",min_value=0.0,max_value=1.0,step=0.05)
+    col_battery_power,col_battery_capacity = st.columns(2)
+    with col_battery_power:
+        max_power = st.slider(value=0.5,label="Absolute power limit battery [MW]",min_value=0.0,max_value=5.0,step=0.05)
+    with col_battery_capacity:
+        capacity_battery = st.slider(value=1.0,label="Battery capacity [MWh]",min_value=0.1,max_value=10.0,step=0.1)
+    max_power_pv = st.slider(value=1.0,label="Maximum production capacity PV [MW]",min_value=0.1,max_value=10.0,step=0.1)
+
+    starting_soc = st.slider(label="Starting state-of-charge (SOC) [%]",min_value=0.0,max_value=100.0,step=0.5)
     etp_cost = st.number_input(label='ETP cost [EUR/MWh]',value=1.0,min_value=0.0)
     col1,col2 = st.columns(2)
     with col1:
@@ -31,17 +41,16 @@ def main():
     
     click = st.button('Run optimization')
     if click:
-        exp = st.expander('Explanation of figures',icon='ℹ️')
-        with exp:
+        with st.expander('Explanation of figures',icon='ℹ️'):
             st.write('From top to bottom:')
             st.markdown("- The first figure shows the State-of-charge (SOC), PV production (PV), the battery power (Power), and the PV production flowing into the batter (PV to battery)")
             st.markdown("- The second figure shows the cumulative P&L for the colocation project, the cumulative P&L that would have been realized with only the PV, and the cumulative P&L that would have been realized with only the specified battery. This P&L does not yet account for the tax- grid-fee- or slippage-related benefits.")
             st.markdown("- The third figure shows the DA prices which were used in the optimization")
             st.markdown("- The fourth figure indicates the accumulated benefit that arises from not having to buy power on an illiquid market by flowing PV to the battery. This KPI is calculated based on the passed average bid-ask spread. The benit originates from PV and the battery 'meeting in the middle' instead of PV selling at a lower price than what the battery would buy at.")
-            
-            
-        fig = optimization.generate_output(starting_soc=starting_soc,p_limit=max_power,charge_from_grid=charge_from_grid,date=date.strftime('%Y-%m-%d'),etp_cost=etp_cost,average_bid_ask_spread=average_bid_ask_spread)
-        st.pyplot(fig)
+        
+        with st.spinner(text='Running optimization...'):   
+            fig = optimization.generate_output(battery_capacity=capacity_battery,max_power_pv=max_power_pv,starting_soc=starting_soc,p_limit=max_power,charge_from_grid=charge_from_grid,date=date.strftime('%Y-%m-%d'),etp_cost=etp_cost,average_bid_ask_spread=average_bid_ask_spread)
+            st.pyplot(fig)
 
 if __name__== '__main__':
     main()
